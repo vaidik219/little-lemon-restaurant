a012184e5064d380949280da12ede4fb2a51e2fc
Vaidik Rungta <rungtavaidik@gmail.com>
2025-05-19 01:24:20 +0530

    Add semantic elements and meta tags

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import logo from "../assets/Logo .svg"
+
+export default function Footer() {
+    return (
+        <>
+            <img src={logo} />
+        </>
+    )
+}
---
+++ src/components/Nav.js
@@ -0,0 +1,9 @@
+import logo from "../assets/Logo .svg"
+
+export default function Nav() {
+    return (
+        <>
+            <img src={logo} />
+        </>
+    )
+}
